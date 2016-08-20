05dbb9807692d65294e21dba5dbddb1254b282dc
Yuval Raz <yuvalzhe@gmail.com>
2016-08-20 21:30:24 +0300

    added photo as background, added social buttons. added bootstrap.

--- yuvalJs.js
+++ yuvalJs.js
@@ -3,29 +3,46 @@ $(document).ready(function(){
     $(".militaryService").hide();
     $(".education").hide();
     $(".skills").hide();
-    
+
+
     $("#workExpOpen").css('cursor', 'pointer').click(function(){
         $(".workExp").slideToggle("slow");
         $("html, body").delay(500).animate({
             scrollTop: $("#workExpOpen").offset().top
         }, 500);
+        $(".militaryService").slideUp();
+        $(".education").slideUp();
+        $(".skills").slideUp();
+
     });
     $("#militaryServiceOpen").css('cursor', 'pointer').click(function(){
         $(".militaryService").slideToggle("slow");
         $("html, body").delay(500).animate({
             scrollTop: $("#militaryServiceOpen").offset().top
         }, 500);
+        $(".workExp").slideUp();
+        $(".education").slideUp();
+        $(".skills").slideUp();
+
+
     });
     $("#educationOpen").css('cursor', 'pointer').click(function(){
         $(".education").slideToggle("slow");
         $("html, body").delay(500).animate({
             scrollTop: $("#educationOpen").offset().top
         }, 500);
+        $(".workExp").slideUp();
+        $(".militaryService").slideUp();
+        $(".skills").slideUp();
+
     });
     $("#skillsOpen").css('cursor', 'pointer').click(function(){
         $(".skills").slideToggle("slow");
         $("html, body").delay(500).animate({
             scrollTop: $("#skillsOpen").offset().top
         }, 500);
+        $(".workExp").slideUp();
+        $(".militaryService").slideUp();
+        $(".education").slideUp();
     });
 });
